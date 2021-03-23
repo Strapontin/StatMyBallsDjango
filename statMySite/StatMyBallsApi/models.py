f62468f59613b11c10851a46ba0a00dccbb01b90
Strapontin <antodu73@hotmail.fr>
2021-03-23 22:16:25 +0100

    Update models. Should now be able to be used

--- statMySite/StatMyBallsApi/models.py
+++ statMySite/StatMyBallsApi/models.py
@@ -3,12 +3,59 @@ from django.contrib.auth.models import User
 from datetime import datetime
 
 
-# Enum For Player position
-class PlayerPosition(models.Model):
-    position = models.CharField(max_length=60)
+# Should not be necessary
+# # Enum For Player position
+# class PlayerPosition(models.Model):
+#     position = models.CharField(max_length=60)
+#
+#     def __str__(self):
+#         return self.position
+
+# Model for each context
+class Contest(models.Model):
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.position
+        template = 'Démarré le {0.start_date}'
+        return template.format(self)
+
+
+# Enum for team colors
+class TeamColor(models.Model):
+    name = models.CharField(max_length=30)
+    rgb_value = CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+# Model for each team
+class Team(models.Model):
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+    team_color = models.ForeignKey(TeamColor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        template = 'Equipe {0.team_color.name} du match démarré le {0.contest.start_date}'
+        return template.format(self)
+
+
+# Model for each player
+class Player(models.Model):
+    name = CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+# Model to describe players in team
+class TeamComposition(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+    def __str__(self):
+        template = "Le joueur {0.player.name} est dans l'équipe {0.team.team_color}"
+        return template.format(self)
 
 
 # Enum For Goal Types
@@ -22,10 +69,12 @@ class GoalType(models.Model):
 # Model for Goals Table
 class Goal(models.Model):
     goal_date = models.DateTimeField('goal_date', auto_now_add=True)
-    player = models.ForeignKey(User, on_delete=models.CASCADE)
-    player_position = models.ForeignKey(PlayerPosition, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    # player_position = models.ForeignKey(PlayerPosition, on_delete=models.CASCADE)
     goal_type = models.ForeignKey(GoalType, on_delete=models.CASCADE)
+    enemy_player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
 
     def __str__(self):
-        template = '{0.player} a marqué {0.goal_type} depuis {0.player_position} à {0.goal_date}'
+        template = '{0.player} a marqué {0.goal_type} face à {0.enemy_player} à {0.goal_date}'
         return template.format(self)
